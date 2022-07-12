bb091965471db1b4f4fffc356f93f9102722ca9c
theander <aclaudino@gmail.com>
2022-07-11 22:47:01 -0300

    First commit

---
+++ src/main/java/com/healthmanagement/labsapi/LabsApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthmanagement.labsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsApiApplication.class, args);
+	}
+
+}
