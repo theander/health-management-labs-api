5b0059be9e3bff812f84c93223e34dc74197d333
theander <aclaudino@gmail.com>
2023-06-26 22:53:42 -0300

    Adicionado Flyway

---
+++ src/main/resources/db/migration/V1__create_migration.sql
@@ -0,0 +1,9 @@
+create schema if not exists labs;
+create table labs.lab (
+                     id  bigserial not null,
+                     description varchar(255),
+                     name varchar(255),
+                     status int4,
+                     username varchar(255),
+                     primary key (id)
+);
